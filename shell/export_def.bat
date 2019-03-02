a98f7583ca5836dfcad380b19c447cc32672fd65
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-03-02 15:59:17 +0800

    png split

---
+++ shell/export_def.bat
@@ -0,0 +1,4 @@
+copy bewater-lovepixel\data\def.js bamboo-lovepixel\assets\script\Def\
+copy bewater-lovepixel\data\opcode.js bamboo-lovepixel\assets\script\Def\
+copy bewater-lovepixel\data\errcode.js bamboo-lovepixel\assets\script\Def\
+pause
---
+++ shell/export_excel.bat
@@ -0,0 +1,3 @@
+python tools/excel/xlsx2lua.py ./excel/ bewater-lovepixel/script/def/prop
+python tools/excel/xlsx2js.py ./excel/ bamboo-lovepixel/assets/script/Def
+pause
